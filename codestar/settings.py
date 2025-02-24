6628bc8df37937d001588676c0cdbb5cc0c3ee27
Fariba-Kamani <fariba.kamani87@gmail.com>
2025-02-24 15:11:08 +0100

    Prepare and update for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j)uhrztti=b-4)!(pulq3ep7pg17*dp7lo)uw@%*fw_mudhg)5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
